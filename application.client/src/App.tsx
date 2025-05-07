c9f2a486c92c0c037bb2ab100f06249ab6a1e3f6
Jhuan Julio Paraan <01ph1714.jhuan.p@ict888.net>
2025-05-07 14:38:33 +0800

    [General] Set up clean architecture folders

--- application.client/src/App.tsx
+++ application.client/src/App.tsx
@@ -1,58 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
-
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+    const [count, setCount] = useState<number>(0);
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
-    );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        if (response.ok) {
-            const data = await response.json();
-            setForecasts(data);
+    const handleCounter = (e: MouseEvent<HTMLButtonElement>) => {
+        if (e?.target?.id === "increment") {
+            setCount((prevCount) => prevCount + 1)
+        } else {
+            setCount((prevCount) => prevCount - 1)
         }
     }
+
+    return (
+        <>
+            <p id="count">{count}</p>
+            <div className="buttons-container">
+                <button id="increment" onClick={handleCounter}>+</button>
+                <button id="decrement" onClick={handleCounter}>-</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
